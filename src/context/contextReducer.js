74f06537c55f4315886e0b949187ca0dc4cc21f5
Akshay <akshaygowda5701@gmail.com>
2023-04-05 03:21:48 +0530

    final commit

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -1,15 +1,22 @@
-const contextReducer = (state, action) =>{
+const contextReducer = (state, action) => {
     let transactions;
-
-    switch (action.type){
-        case 'DELETE_TRANSACTION':
-            transactions = state.filter((t)=> t.id !== action.payload);
-
-            return transactions;
-        case 'ADD_TRANSACTION':
-            transactions = [action.payload, ...state];
-        
-            return transactions;
-        
+  
+    switch (action.type) {
+      case 'DELETE_TRANSACTION':
+        transactions = state.filter((transaction) => transaction.id !== action.payload);
+  
+        localStorage.setItem('transactions', JSON.stringify(transactions));
+  
+        return transactions;
+      case 'ADD_TRANSACTION':
+        transactions = [action.payload, ...state];
+  
+        localStorage.setItem('transactions', JSON.stringify(transactions));
+  
+        return transactions;
+      default:
+        return state;
     }
-}
+  };
+  
+  export default contextReducer;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { SpeechProvider } from '@speechly/react-client';
+import { SpeechProvider } from '@speechly/react-client';
 
 import { Provider } from './context/context';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  // <SpeechProvider appId="7c4aee08-1073-4a32-b862-ebe1850e0732" language="en-US">
+  <SpeechProvider appId="key..." language="en-US">
     <Provider>
       <App />
-    </Provider>,
-  // </SpeechProvider>,
+    </Provider>
+  </SpeechProvider>,
   document.getElementById('root'),
 );
